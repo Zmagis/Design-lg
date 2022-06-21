e270f69bcfe88f8ca2ccef58a9f1ecae121db98a
Eglė Gustaitė <egle@vertex.lt>
2022-06-21 22:15:50 +0300

    medium screen css

--- src/components/cards/cardData.ts
+++ src/components/cards/cardData.ts
@@ -1,88 +1,67 @@
 import { CardData } from '../../types/card';
 
+const list = [
+  {
+    id: 1,
+    avatarUrl: '',
+    title: 'Lorem ipsum',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
+  },
+  {
+    id: 2,
+    avatarUrl: '',
+    title: 'Lorem ipsum',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
+  },
+  {
+    id: 3,
+    avatarUrl: '',
+    title: 'Lorem ipsum',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
+  },
+  {
+    id: 4,
+    avatarUrl: '',
+    title: 'Lorem ipsum',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
+  },
+  {
+    id: 5,
+    avatarUrl: '',
+    title: 'Lorem ipsum',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
+  },
+  {
+    id: 6,
+    avatarUrl: '',
+    title: 'Lorem ipsum',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
+  },
+];
+
 export const cardsData: CardData[] = [
   {
     id: 1,
     description: 'Lorem ipsum dolor sit amet, consectetur Lorem ipsum',
     descritionDetail: 'Lorem ipsum dolor sit',
-    list: [
-      {
-        id: 1,
-        avatarUrl: '',
-        title: 'Lorem ipsum',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
-      },
-      {
-        id: 2,
-        avatarUrl: '',
-        title: 'Lorem ipsum',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
-      },
-      {
-        id: 3,
-        avatarUrl: '',
-        title: 'Lorem ipsum',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
-      },
-    ],
+    list,
   },
   {
     id: 2,
     description: 'Lorem ipsum dolor sit amet, consectetur Lorem ipsum',
     descritionDetail: 'Lorem ipsum dolor sit',
-    list: [
-      {
-        id: 1,
-        avatarUrl: '',
-        title: 'Lorem ipsum',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
-      },
-      {
-        id: 2,
-        avatarUrl: '',
-        title: 'Lorem ipsum',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
-      },
-      {
-        id: 3,
-        avatarUrl: '',
-        title: 'Lorem ipsum',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
-      },
-    ],
+    list,
   },
   {
     id: 3,
     description: 'Lorem ipsum dolor sit amet, consectetur Lorem ipsum',
     descritionDetail: 'Lorem ipsum dolor sit',
-    list: [
-      {
-        id: 1,
-        avatarUrl: '',
-        title: 'Lorem ipsum',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
-      },
-      {
-        id: 2,
-        avatarUrl: '',
-        title: 'Lorem ipsum',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
-      },
-      {
-        id: 3,
-        avatarUrl: '',
-        title: 'Lorem ipsum',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
-      },
-    ],
+    list,
   },
 ];
